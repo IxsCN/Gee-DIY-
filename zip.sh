a898043b76efb23763c8d2b07cc020524375e79c
IxsCN <Ixs.CN@hotmail.com>
2016-11-21 22:18:57 +0800

    upload project

---
+++ zip.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+# Copyright (C) 2016 evenS
+tar -czvf /tmp/data/backgroud/backgroud.tar.gz /usr/lib/lua/luci/controller/app/backgroud.lua /usr/lib/lua/luci/view/app/backgroud.htm /www/turbo-static/turbo/v2/js/backgroud.js
+
