6e31f0cdb0c2fe231e7076afcdf7312c530e80e0
IxsCN <Ixs.CN@hotmail.com>
2017-05-20 22:43:01 +0800

    修复bug
    修正login页无法立即生效的问题，完善打包脚本，现在插件强制依赖外部存储。

--- zip.sh
+++ zip.sh
@@ -2,3 +2,4 @@
 # Copyright (C) 2016 evenS
 tar -czvf /tmp/data/backgroud/backgroud.tar.gz /usr/lib/lua/luci/controller/app/backgroud.lua /usr/lib/lua/luci/view/app/backgroud.htm /www/turbo-static/turbo/v2/js/backgroud.js
 
+tar -czvf /tmp/data/backgroud/archive.tgz -C /tmp/data/backgroud/ manifest.json script default.jpg backgroud.tar.gz
